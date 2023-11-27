a4402d4ec072f19bbf1a1763a2f4e5e063abe4af
Biehler1 <103413662+Biehler1@users.noreply.github.com>
2023-11-27 11:19:41 -0800

    new grade.sh

--- grade.sh
+++ grade.sh
@@ -1,3 +1,4 @@
+#Since for windows there are two separate CPATHs for compiling and running JUnit tests.
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
@@ -8,9 +9,35 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+# Checks if ListExamples.java exists.
+# If it doesn't send an error.
+if [ ! -e "student-submission/ListExamples.java" ]; then 
+    echo "Error: No ListExamples.java file found."
+    exit 1
+fi
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+# Copies ListExamples.java, TestListExamples.java, and all JUnit files in lib to grading-area
+cp student-submission/ListExamples.java grading-area/
+cp TestListExamples.java grading-area/
+cp -r lib grading-area/
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+# Compiles JUnit Tests and sees if there is an error code
+# If so, send an error.
+cd grading-area
+javac -cp $CPATH *.java
+
+if [ $? -ne 0 ]; then
+    echo "Error: Compilation error."
+    exit 1
+fi
+
+# Runs JUnit Tests and redirects all output into test-results.txt
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test-results.txt 2>&1
+
+# Checks if there is any FAILURES!!! from the JUnit test.
+# If so, tells the user that tests failed
+if grep "FAILURES!!!" test-results.txt; then
+    echo "Tests failed. See test-results.txt for details."
+else
+    echo "All tests passed."
+fi
